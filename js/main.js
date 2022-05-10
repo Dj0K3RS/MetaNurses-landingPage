535d11812405526c3a5ea934ae83f952094aa0f4
Dj0k3rs <adambenmessaoud2@gmail.com>
2022-05-10 01:18:37 +0100

    second update

--- js/main.js
+++ js/main.js
@@ -11,108 +11,95 @@
 var $ = jQuery.noConflict();
 
 
-$(document).ready(function(){
-    $('.collapse').on('show.bs.collapse', function (e) {
-        $('.collapse').collapse("hide")
-    });
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        autoplay:true,
- 
-    autoplayHoverPause:true,
+$(document).ready(function () {
+  $('.collapse').on('show.bs.collapse', function (e) {
+    $('.collapse').collapse("hide")
+  });
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+
+    autoplayHoverPause: true,
     animateOut: 'fadeOut',
-    autoPlayTimeout:100,
-    autoplaySpeed:6000,
-        responsive:{
-            0:{
-                items:2
-            },
-            600:{
-                items:4
-            },
-            1000:{
-                items:5
-            }
-        }
+    autoPlayTimeout: 100,
+    autoplaySpeed: 6000,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 4
+      },
+      1000: {
+        items: 7
+      }
+    }
+  });
+
+
+  /*Roadmap*/
+  /*roadmap*/
+  function qs(selector, all = false) {
+    return all ? document.querySelectorAll(selector) : document.querySelector(selector)
+  }
+
+  const sections = qs('.section', true);
+  const timeline = qs('.timeline');
+  const line = qs('.line');
+  line.style.bottom = `calc(100% - 20px)`;
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * 0.8;
+
+  function scrollHandler(e) {
+    const {
+      scrollY
+    } = window;
+    up = scrollY < prevScrollY;
+    down = !up;
+    const timelineRect = timeline.getBoundingClientRect();
+    const lineRect = line.getBoundingClientRect(); //CONST LINEHEIGHT = lineRect.bottom - lineRect.top
+
+    const dist = targetY - timelineRect.top
+    //  console.log(dist);
+
+    if (down && !full) {
+      set = Math.max(set, dist);
+      line.style.bottom = `calc(100% - ${set}px)`
+    }
+
+    if (dist > timeline.offsetHeight + 50 && !full) {
+      full = true;
+      line.style.bottom = `-50px`
+    }
+
+    sections.forEach(item => {
+      //console.log(items);
+      const rect = item.getBoundingClientRect();
+
+      if (rect.top + item.offsetHeight / 5 < targetY) {
+        item.classList.add('show-me')
+      }
     });
 
+    prevScrollY = window.scrollY;
+  }
 
-    /*Roadmap*/
-        /*roadmap*/
-        function qs(selector, all = false) {
-            return all ? document.querySelectorAll(selector) : document.querySelector(selector)
-        }
-        
-        const sections = qs('.section', true);
-        const timeline = qs('.timeline');
-        const line = qs('.line');
-        line.style.bottom = `calc(100% - 20px)`;
-        let prevScrollY = window.scrollY;
-        let up, down;
-        let full = false;
-        let set = 0;
-        const targetY = window.innerHeight * 0.8;
-        
-        function scrollHandler(e){
-            const{
-                scrollY
-            } = window;
-            up = scrollY < prevScrollY;
-            down = !up;
-            const timelineRect = timeline.getBoundingClientRect();
-            const lineRect = line.getBoundingClientRect(); //CONST LINEHEIGHT = lineRect.bottom - lineRect.top
-        
-            const dist = targetY - timelineRect.top
-          //  console.log(dist);
-        
-            if (down && !full){
-                set = Math.max(set, dist);
-                    line.style.bottom = `calc(100% - ${set}px)`
-            }
-        
-            if (dist > timeline.offsetHeight + 50 && !full){
-                full = true;
-                line.style.bottom = `-50px`
-            }
-        
-            sections.forEach(item => {
-                //console.log(items);
-                const rect = item.getBoundingClientRect();
-        
-                if(rect.top + item.offsetHeight / 5 < targetY) {
-                    item.classList.add('show-me')
-                }
-            });
-        
-            prevScrollY = window.scrollY;
-        }
-        
-        scrollHandler();
-        line.style.display = 'block';
-        window.addEventListener('scroll', scrollHandler);
-      
-      
-      
-        
-       /*back to top*/
-      
-       //Get the button:
-      mybutton = document.getElementById("myBtn");
-      
-      // When the user scrolls down 20px from the top of the document, show the button
-      window.onscroll = function() {scrollFunction(),myFunction()};
-      
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          mybutton.style.display = "block";
-        } else {
-          mybutton.style.display = "none";
-        }
-      }
-      
-       // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  scrollHandler();
+  line.style.display = 'block';
+  window.addEventListener('scroll', scrollHandler);
+
+
+
+
+
+
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -126,40 +113,29 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
+      }, 800, function () {
+
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
   });
-      
-      
+
+
 
   /*MENU HOVER*/
 
-// When the user scrolls the page, execute myFunction
+  // When the user scrolls the page, execute myFunction
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {};
-
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    alert("ok");
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+  // When the user scrolls the page, execute myFunction
 
 
-  
+
+
+
+
+
+
 });
+
+/*carousel*/
